9dede67e003c87710f18ecf7befb2e969b67b1db
Vikash <vikashkumargupta951@gmail.com>
2025-03-13 20:41:56 +0530

    initialization

--- src/app.js
+++ src/app.js
@@ -4,9 +4,36 @@ const express = require('express');
 //this create instance of an express application
 const app = express();
 
-app.use((req , res) => {
-    res.send("hello from nodejs server")
+
+
+app.use("/hello" , (req , res) =>{
+    res.send("hello page ki entry hogai")
 })
+
+
+//this will match all the HTTP method API calls to /test
+app.use("/test" , (req , res) =>{
+    res.send("test page")
+})
+
+//this will only handle GET call to /user
+app.get("/user" , (req , res) =>{
+    res.send({"firstName" : "Vikash" , "lastName" : "Gupta"} )
+})
+
+app.post("/user" , (req , res) =>{
+    res.send("user database updated successfully")
+})
+
+app.delete("/user" , (req , res) =>{
+    res.send("user deleted successfully")
+})
+
+app.use("/" ,(req , res) => {
+    res.send("home page")
+})
+
+
 
 //
 app.listen(3000 , () => {
